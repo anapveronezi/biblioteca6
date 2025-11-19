82fe5f1a05cce8b45ee02a38931cd90d6eb8c35b
anapveronezi <ana.veronezi@aluno.unifenas.br>
2025-11-18 21:23:05 -0300

    chore: inicializa projeto biblioteca

---
+++ src/main/java/com/biblioteca/biblioteca6/Biblioteca6Application.java
@@ -0,0 +1,13 @@
+package com.biblioteca.biblioteca6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Biblioteca6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Biblioteca6Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/biblioteca/biblioteca6/Biblioteca6ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.biblioteca.biblioteca6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Biblioteca6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
